c7593771ea2548d7dea5b8521497dc5c7d801a77
wirtanen <u_key7@hotmail.com>
2015-09-08 18:55:36 -0700

    Drone 3
    Added drone 3 component to the client side code

--- www/js/control.js
+++ www/js/control.js
@@ -1,27 +1,51 @@
 
         var socket1 = io.connect("http://192.168.3.100:8080");
         var socket2 = io.connect("http://192.168.3.101:8080");
+        var socket3 = io.connect("http://192.168.3.102:8080");
+        var status1 = false;
+        var status2 = false;
+        var status3 = false;
+        
 
         socket1.on('connect', function(){
           console.log('Socket1 Connected');
-          statusON();
+          status1=true;
         });
 
         socket2.on('connect', function(){
           console.log('Socket2 Connected');
-          statusOFF();
+          status2=true;
         });
+
+        socket3.on('connect', function(){
+            console.log('Socket3 Connected');
+            status3=true;
+        });
+        
+        if(status1 && status2 && status3)
+        {
+            statusON();
+        }
+        else
+        {
+            statusOFF();
+        }
 
         function getsocket(id){
           switch(id) {
-            case "drone1":
-            case      "d1":
+            case 'drone1':
+            case     'd1':
                 return socket1;
                 break;
 
             case 'drone2':
-            case      'd2':
+            case     'd2':
                 return socket2;
+                break;
+                  
+            case 'drone3':
+            case     'd3':
+                return socket3;
                 break;
           };
         }
@@ -31,9 +55,11 @@
             case 'drone1':
               return 'd1';
               break;
+                  
             case 'drone2':
               return 'd2';
               break;
+                  
             case 'drone3':
               return 'd3';
               break;
